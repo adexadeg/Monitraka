b2b0cc22e4c2c8bf2fbdd0d7ab8ee75f70b2b497
Adelu Adegoke <adeluadegoke21@gmail.com>
2022-06-20 10:42:58 +0100

    first commit

---
+++ users/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractBaseUser
+
+# from user.managers import UserManager
+# from .managers import UserManager
+
+
+# Create your models here.
+
+
+class User(AbstractBaseUser):
+    fullname = models.CharField(max_length=500, blank=True, null=True)
+    email = models.EmailField(max_length=255, unique= True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    password = models.CharField(max_length=255, null=True)
+    how_did_you_hear_about_us = models.Choices
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    # lastname= models.CharField(max_length=255, blank=True, null=True)
+    # image = models.FileField(upload_to='users/', blank=True, null=True)
+    
+    # last_login = models.DateField(null=True)
+    # is_verified = models.BooleanField(null=True)
+    # is_active = models.BooleanField(default=True)
+    # is_staff = models.BooleanField(default=False)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
+    
+#     objects = UserManager()
+
+
+
+#     def __str__(self):
+#         return f'{self.firstname} {self.email}'
+
+#     class Meta:
+#         ordering = ('-created_at',)
+
+
